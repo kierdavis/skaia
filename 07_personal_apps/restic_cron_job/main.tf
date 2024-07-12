0dc02666f41c2c0146ad560f3efc182c3289a598
Kier Davis <me@kierdavis.com>
2024-07-12 17:06:45 +0100

    set up backups using restic directly

---
+++ 07_personal_apps/restic_cron_job/main.tf
@@ -0,0 +1,96 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+variable "name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "schedule" {
+  type = string
+}
+
+variable "pvc_name" {
+  type = string
+}
+
+variable "mount_path" {
+  type = string
+}
+
+variable "archive_secret_name" {
+  type = string
+}
+
+locals {
+  globals = yamldecode(file("${path.module}/../../globals.yaml"))
+}
+
+resource "kubernetes_cron_job_v1" "main" {
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+    labels    = { "app.kubernetes.io/name" = var.name }
+  }
+  spec {
+    concurrency_policy            = "Forbid"
+    failed_jobs_history_limit     = 1
+    schedule                      = var.schedule
+    starting_deadline_seconds     = 6 * 60 * 60
+    successful_jobs_history_limit = 1
+    job_template {
+      metadata {
+        labels = { "app.kubernetes.io/name" = var.name }
+      }
+      spec {
+        backoff_limit = 0
+        template {
+          metadata {
+            labels = { "app.kubernetes.io/name" = var.name }
+          }
+          spec {
+            restart_policy = "Never"
+            container {
+              name  = "main"
+              image = "docker.io/restic/restic@sha256:157243d77bc38be75a7b62b0c00453683251310eca414b9389ae3d49ea426c16"
+              args = [
+                "backup",
+                "--exclude=lost+found",
+                "--exclude=.nobackup",
+                "--host=skaia",
+                "--one-file-system",
+                "--read-concurrency=4",
+                "--repo=b2:${local.globals.b2.archive.bucket}:/skaia/personal-1",
+                var.mount_path,
+              ]
+              env_from {
+                secret_ref {
+                  name = var.archive_secret_name
+                }
+              }
+              volume_mount {
+                name       = "data"
+                mount_path = var.mount_path
+                read_only  = true
+              }
+            }
+            volume {
+              name = "data"
+              persistent_volume_claim {
+                claim_name = var.pvc_name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- 07_personal_apps/storage/main.tf
+++ 07_personal_apps/storage/main.tf
@@ -68,6 +68,26 @@ resource "kubernetes_secret" "archive" {
   }
 }
 
+module "media_backup" {
+  source              = "../restic_cron_job"
+  name                = "media-backup"
+  namespace           = var.namespace
+  schedule            = "0 2 * * 2"
+  pvc_name            = kubernetes_persistent_volume_claim.media.metadata[0].name
+  mount_path          = "/net/skaia/media"
+  archive_secret_name = kubernetes_secret.archive.metadata[0].name
+}
+
+module "projects_backup" {
+  source              = "../restic_cron_job"
+  name                = "projects-backup"
+  namespace           = var.namespace
+  schedule            = "0 2 * * *"
+  pvc_name            = kubernetes_persistent_volume_claim.projects.metadata[0].name
+  mount_path          = "/net/skaia/projects"
+  archive_secret_name = kubernetes_secret.archive.metadata[0].name
+}
+
 output "downloads_pvc_name" {
   value = kubernetes_persistent_volume_claim.downloads.metadata[0].name
 }
@@ -83,3 +103,35 @@ output "projects_pvc_name" {
 output "archive_secret_name" {
   value = kubernetes_secret.archive.metadata[0].name
 }
+
+#resource "kubernetes_job" "restic_init" {
+#  metadata {
+#    name = "restic-init"
+#    namespace = var.namespace
+#    labels = { "app.kubernetes.io/name" = "restic-init" }
+#  }
+#  spec {
+#    backoff_limit = 0
+#    template {
+#      metadata {
+#        labels = { "app.kubernetes.io/name" = "restic-init" }
+#      }
+#      spec {
+#        restart_policy = "Never"
+#        container {
+#          name = "main"
+#          image = "docker.io/restic/restic@sha256:157243d77bc38be75a7b62b0c00453683251310eca414b9389ae3d49ea426c16"
+#          args = [
+#            "init",
+#            "--repo=b2:${local.globals.b2.archive.bucket}:/skaia/personal-1",
+#          ]
+#          env_from {
+#            secret_ref {
+#              name = kubernetes_secret.archive.metadata[0].name
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
