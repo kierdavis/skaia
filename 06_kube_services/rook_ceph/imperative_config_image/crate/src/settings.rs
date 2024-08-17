f6e6310ec1ba81b3d2cbd89e460ce19dc40bdd78
Kier Davis <me@kierdavis.com>
2024-08-17 15:47:19 +0100

    attempt to reduce ceph mds memory usage

--- 06_kube_services/rook_ceph/imperative_config_image/crate/src/settings.rs
+++ 06_kube_services/rook_ceph/imperative_config_image/crate/src/settings.rs
@@ -3,6 +3,7 @@ use std::process::{Command, Stdio};
 
 pub fn configure() -> Result<(), Error> {
   Namespace::Global.ensure_with_context("bluestore_compression_mode", "aggressive")?;
+  Namespace::Global.ensure_with_context("mds_cache_memory_limit", "268435456")?;
   Namespace::Global.ensure_with_context("osd_deep_scrub_interval", "9676800.000000")?;
   Namespace::Global.ensure_with_context("osd_memory_target", "1073741824")?;
   Namespace::Global.ensure_with_context("osd_pool_default_size", "2")?;
