99d23ab7defcb87e9f88bc653d224b6c66ede08a
Kier Davis <me@kierdavis.com>
2024-10-19 02:34:12 +0100

    do some transcoding

--- 07_personal_apps/adhoc.tf
+++ 07_personal_apps/adhoc.tf
@@ -1,92 +1,119 @@
-#resource "kubernetes_job" "transcode" {
-#  metadata {
-#    name = "transcode"
-#    namespace = kubernetes_namespace.main.metadata[0].name
-#    labels = { app = "transcode" }
-#  }
-#  spec {
-#    backoff_limit = 0
-#    template {
-#      metadata {
-#        labels = { app = "transcode" }
-#      }
-#      spec {
-#        restart_policy = "Never"
-#        affinity {
-#          node_affinity {
-#            # This node has a Skylake CPU while pyrope has a Broadwell.
-#            required_during_scheduling_ignored_during_execution {
-#              node_selector_term {
-#                match_expressions {
-#                  key      = "kubernetes.io/hostname"
-#                  operator = "In"
-#                  values   = ["vantas"]
-#                }
-#              }
-#            }
-#          }
-#        }
-#        volume {
-#          name = "media"
-#          persistent_volume_claim {
-#            claim_name = module.storage.media_pvc_name
-#          }
-#        }
-#        volume {
-#          name = "downloads"
-#          persistent_volume_claim {
-#            claim_name = module.storage.downloads_pvc_name
-#          }
-#        }
-#        container {
-#          name = "main"
-#          image = "nixos/nix"
-#          command = [
-#            "bash", "-c",
-#            <<-EOS
-#              set -o errexit -o nounset -o pipefail
-#              mkdir -p /run/opengl-driver/lib/dri
-#              ln -sf $(nix-build '<nixpkgs>' -A intel-media-driver --no-out-link)/lib/dri/iHD_drv_video.so /run/opengl-driver/lib/dri/iHD_drv_video.so
-#              nix-env -i -A nixpkgs.ffmpeg-full -A nixpkgs.libva-utils
-#              vainfo --display drm --device /dev/dri/renderD128
-#              ffmpeg \
-#                -y \
-#                -init_hw_device qsv=hw \
-#                -filter_hw_device hw \
-#                -i '/net/skaia/media/foobar.mkv' \
-#                -vf format=nv12,hwupload=extra_hw_frames=64 \
-#                -map 0 \
-#                -c:a copy \
-#                -c:s copy \
-#                -c:v h264_qsv \
-#                '/net/skaia/media/foobar.out.mkv'
-#            EOS
-#          ]
-#          volume_mount {
-#            name       = "media"
-#            mount_path = "/net/skaia/media"
-#          }
-#          volume_mount {
-#            name       = "downloads"
-#            mount_path = "/net/skaia/torrent-downloads"
-#            read_only  = true
-#          }
-#          resources {
-#            requests = {
-#              cpu               = "1"
-#              memory            = "2Gi"
-#              "squat.ai/render" = "1"
-#            }
-#            limits = {
-#              "squat.ai/render" = "1"
-#            }
-#          }
-#        }
-#      }
-#    }
-#  }
-#  wait_for_completion = false
-#}
+resource "kubernetes_job" "transcode" {
+  for_each = {
+    #s01e05 = "/net/skaia/media/foobar/S01E05.mp4"
+    #s01e06 = "/net/skaia/media/foobar/S01E06.mp4"
+    #s01e01 = "/net/skaia/media/foobar/S01E01.mp4"
+    #s01e02 = "/net/skaia/media/foobar/S01E02.mp4"
+    #s01e03 = "/net/skaia/media/foobar/S01E03.mp4"
+    #s01e04 = "/net/skaia/media/foobar/S01E04.mp4"
+  }
+  metadata {
+    name      = "transcode-${each.key}"
+    namespace = kubernetes_namespace.main.metadata[0].name
+    labels    = { app = "transcode", instance = each.key }
+  }
+  spec {
+    backoff_limit = 0
+    template {
+      metadata {
+        labels = { app = "transcode", instance = each.key }
+      }
+      spec {
+        restart_policy = "Never"
+        affinity {
+          node_affinity {
+            # Require a node with Intel QSV (all QSV versions include h.264 encoding).
+            required_during_scheduling_ignored_during_execution {
+              node_selector_term {
+                match_expressions {
+                  key      = "kubernetes.io/hostname"
+                  operator = "In"
+                  values   = ["vantas", "pyrope"]
+                }
+              }
+            }
+          }
+          pod_anti_affinity {
+            required_during_scheduling_ignored_during_execution {
+              topology_key = "kubernetes.io/hostname"
+              label_selector {
+                match_expressions {
+                  key      = "app"
+                  operator = "In"
+                  values   = ["transcode"]
+                }
+              }
+            }
+          }
+        }
+        volume {
+          name = "media"
+          persistent_volume_claim {
+            claim_name = module.storage.media_pvc_name
+          }
+        }
+        volume {
+          name = "downloads"
+          persistent_volume_claim {
+            claim_name = module.storage.downloads_pvc_name
+          }
+        }
+        container {
+          name  = "main"
+          image = "nixos/nix"
+          command = [
+            "bash", "-c",
+            <<-EOS
+              set -o errexit -o nounset -o pipefail
+              mkdir -p /run/opengl-driver/lib/dri
+              ln -sf $(nix-build '<nixpkgs>' -A intel-media-driver --no-out-link)/lib/dri/iHD_drv_video.so /run/opengl-driver/lib/dri/iHD_drv_video.so
+              nix-env -i -A nixpkgs.ffmpeg-full -A nixpkgs.libva-utils
+              vainfo --display drm --device /dev/dri/renderD128
+              TMP_FILE="$(dirname "$FILE")/transcodetmp.$(basename "$FILE")"
+              ffmpeg \
+                -y \
+                -init_hw_device qsv=hw \
+                -filter_hw_device hw \
+                -i "$FILE" \
+                -vf format=nv12,hwupload=extra_hw_frames=64 \
+                -map 0 -map -0:4 \
+                -c:a copy \
+                -c:s copy \
+                -c:v h264_qsv \
+                "$TMP_FILE"
+              rm -v "$FILE"
+              mv -v "$TMP_FILE" "$FILE"
+            EOS
+          ]
+          env {
+            name  = "FILE"
+            value = each.value
+          }
+          volume_mount {
+            name       = "media"
+            mount_path = "/net/skaia/media"
+          }
+          volume_mount {
+            name       = "downloads"
+            mount_path = "/net/skaia/torrent-downloads"
+            read_only  = true
+          }
+          resources {
+            requests = {
+              cpu               = "1"
+              memory            = "2Gi"
+              "squat.ai/render" = "1"
+            }
+            limits = {
+              "squat.ai/render" = "1"
+            }
+          }
+        }
+      }
+    }
+  }
+  wait_for_completion = false
+}
 
 #resource "kubernetes_job" "upload_takeout" {
 #  metadata {
