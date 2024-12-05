4f452899ffa2b5673d07cfee7f8f7f3b099ad865
Kier Davis <me@kierdavis.com>
2024-12-05 14:44:06 +0000

    more ad-hoc archive jobs

--- 07_personal_apps/adhoc.tf
+++ 07_personal_apps/adhoc.tf
@@ -70,7 +70,11 @@ resource "kubernetes_job" "archive_download" {
 resource "kubernetes_job" "archive_upload" {
   for_each = {
     # time format: "2012-11-01 22:08:41", or null for current time
-    #quartus = { path = "resources/quartus-13.0sp1-linux", time = null }
+    radial2014logs   = { path = "mc/servers/radialcraft/logs", time = "2014-08-20 14:28:47" }
+    radial2014server = { path = "mc/servers/radialcraft/server", time = "2014-07-31 21:49:00" }
+    radial2014worlds = { path = "mc/servers/radialcraft/worlds", time = "2014-07-30 01:35:55" }
+    documents2017    = { path = "documents", time = "2017-02-12 17:10:47" }
+    projects2018     = { path = "projects/stale", time = "2018-05-23 15:00:40" }
   }
   wait_for_completion = false
   metadata {
@@ -86,20 +90,34 @@ resource "kubernetes_job" "archive_upload" {
       }
       spec {
         restart_policy = "Never"
+        # Hacky jobqueue setup:
         affinity {
           node_affinity {
-            preferred_during_scheduling_ignored_during_execution {
-              weight = 50
-              preference {
+            required_during_scheduling_ignored_during_execution {
+              node_selector_term {
                 match_expressions {
-                  key      = "topology.kubernetes.io/zone"
+                  key      = "kubernetes.io/hostname"
                   operator = "In"
-                  values   = ["z-adw"]
+                  values   = ["pyrope"]
                 }
               }
             }
           }
         }
+        #affinity {
+        #  node_affinity {
+        #    preferred_during_scheduling_ignored_during_execution {
+        #      weight = 50
+        #      preference {
+        #        match_expressions {
+        #          key      = "topology.kubernetes.io/zone"
+        #          operator = "In"
+        #          values   = ["z-adw"]
+        #        }
+        #      }
+        #    }
+        #  }
+        #}
         volume {
           name = "scratch"
           persistent_volume_claim {
@@ -135,11 +153,188 @@ resource "kubernetes_job" "archive_upload" {
             run_as_user  = local.globals.personal_uid
             run_as_group = local.globals.personal_uid
           }
+          # Hacky jobqueue setup:
+          resources {
+            requests = {
+              "squat.ai/render" = "1"
+            }
+            limits = {
+              "squat.ai/render" = "1"
+            }
+          }
         }
       }
     }
   }
 }
+
+#resource "kubernetes_job" "archive_xfer" {
+#  wait_for_completion = false
+#  metadata {
+#    name = "archive-xfer"
+#    namespace = kubernetes_namespace.main.metadata[0].name
+#    labels = { app = "archive-xfer" }
+#  }
+#  spec {
+#    backoff_limit = 0
+#    template {
+#      metadata {
+#        labels = { app = "archive-xfer" }
+#      }
+#      spec {
+#        restart_policy = "Never"
+#        # Hacky jobqueue setup:
+#        affinity {
+#          node_affinity {
+#            required_during_scheduling_ignored_during_execution {
+#              node_selector_term {
+#                match_expressions {
+#                  key      = "kubernetes.io/hostname"
+#                  operator = "In"
+#                  values   = ["pyrope"]
+#                }
+#              }
+#            }
+#          }
+#        }
+#        #affinity {
+#        #  node_affinity {
+#        #    required_during_scheduling_ignored_during_execution {
+#        #      node_selector_term {
+#        #        match_expressions {
+#        #          key      = "topology.kubernetes.io/zone"
+#        #          operator = "NotIn"
+#        #          values   = ["z-adw"]
+#        #        }
+#        #      }
+#        #    }
+#        #  }
+#        #}
+#        container {
+#          name = "main"
+#          image = "docker.io/restic/restic@sha256:157243d77bc38be75a7b62b0c00453683251310eca414b9389ae3d49ea426c16"
+#          command = [
+#            "sh",
+#            "-c",
+#            <<-EOF
+#              set -o errexit -o nounset -o pipefail -o xtrace
+#              srcrepo=b2:redactedbucket:skaia/stash-0/personal/media
+#              destrepo=b2:redactedbucket:personal-restic
+#              destpath=/data/media
+#              restic -r "$srcrepo" snapshots --json > snapshots.json
+#              n_snaps=$(jq length snapshots.json)
+#              for snap_idx in $(seq 0 $((n_snaps-1))); do
+#                n_paths=$(jq --argjson snap_idx "$snap_idx" '.[$snap_idx].paths|length' snapshots.json)
+#                if [[ "$n_paths" -ne 1 ]]; then
+#                  echo >&2 "snapshot number $snap_idx does not have exactly one path!"
+#                  exit 1
+#                fi
+#                srcsnap=$(jq --argjson snap_idx "$snap_idx" -r '.[$snap_idx].id' snapshots.json)
+#                srcpath=$(jq --argjson snap_idx "$snap_idx" -r '.[$snap_idx].paths[0]' snapshots.json)
+#                snaptime=$(jq --argjson snap_idx "$snap_idx" -r '.[$snap_idx].time' snapshots.json)
+#                snaptime=$${snaptime/T/ }
+#                snaptime=$${snaptime%Z}
+#                snaptime=$${snaptime%.*}
+#                rm -rf "$destpath"
+#                restic -r "$srcrepo" restore "$srcsnap:$srcpath" --target "$destpath"
+#                restic -r "$destrepo" backup --host=generic --one-file-system --read-concurrency=4 --time="$snaptime" "$destpath"
+#              done
+#            EOF
+#          ]
+#          env_from {
+#            secret_ref {
+#              name = module.storage.archive_secret_name
+#            }
+#          }
+#          # for restic
+#          env {
+#            name = "TZ"
+#            value = "UTC"
+#          }
+#          # Hacky jobqueue setup:
+#          resources {
+#            requests = {
+#              "squat.ai/render" = "1"
+#            }
+#            limits = {
+#              "squat.ai/render" = "1"
+#            }
+#          }
+#          volume_mount {
+#            name = "workspace"
+#            mount_path = "/data"
+#          }
+#        }
+#        volume {
+#          name = "workspace"
+#          ephemeral {
+#            volume_claim_template {
+#              metadata {
+#                labels = { app = "archive-xfer" }
+#              }
+#              spec {
+#                access_modes       = ["ReadWriteOnce"]
+#                storage_class_name = "blk-media0"
+#                resources {
+#                  requests = { storage = "40Gi" }
+#                }
+#              }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
+
+resource "kubernetes_job" "sleep" {
+  wait_for_completion = false
+  metadata {
+    name      = "sleep"
+    namespace = kubernetes_namespace.main.metadata[0].name
+    labels    = { app = "sleep" }
+  }
+  spec {
+    backoff_limit = 0
+    template {
+      metadata {
+        labels = { app = "sleep" }
+      }
+      spec {
+        restart_policy = "Never"
+        # Hacky jobqueue setup:
+        affinity {
+          node_affinity {
+            required_during_scheduling_ignored_during_execution {
+              node_selector_term {
+                match_expressions {
+                  key      = "kubernetes.io/hostname"
+                  operator = "In"
+                  values   = ["pyrope"]
+                }
+              }
+            }
+          }
+        }
+        container {
+          name    = "main"
+          image   = "docker.io/busybox"
+          command = ["sleep", "39600"]
+          # Hacky jobqueue setup:
+          resources {
+            requests = {
+              "squat.ai/render" = "1"
+            }
+            limits = {
+              "squat.ai/render" = "1"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
 
 resource "kubernetes_job" "delete_trash" {
   count               = 0
