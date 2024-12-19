3494656261f0dd11ecdf35c583c36bf9223b99a8
Kier Davis <me@kierdavis.com>
2024-12-19 22:23:23 +0000

    jellyfin image: consistency

--- 07_personal_apps/jellyfin/image.nix
+++ 07_personal_apps/jellyfin/image.nix
@@ -11,34 +11,31 @@ imageTools.append {
     nativeBuildInputs = [ curl ];
     phases = [ "fetchPhase" ];
     fetchPhase = ''
-      mkdir -p $out/debs
-      cd $out/debs
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-compute-runtime/intel-opencl-icd_23.43.27642.40-1ubuntu3_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/l/llvm-toolchain-14/libclang-cpp14t64_14.0.6-19build4_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-graphics-compiler/libigc1_1.0.15468.25-2build1_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-graphics-compiler/libigdfcl1_1.0.15468.25-2build1_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-gmmlib/libigdgmm12_22.3.17%2bds1-1_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/l/llvm-toolchain-14/libllvm14t64_14.0.6-19build4_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/s/spirv-llvm-translator-14/libllvmspirvlib14_14.0.0-12build1_amd64.deb
-      curl --silent --show-error --fail --remote-name \
-        http://archive.ubuntu.com/ubuntu/pool/universe/o/opencl-clang-14/libopencl-clang14_14.0.0-4build2_amd64.deb
+      urls=(
+        'http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-compute-runtime/intel-opencl-icd_23.43.27642.40-1ubuntu3_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-gmmlib/libigdgmm12_22.3.17%2bds1-1_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-graphics-compiler/libigc1_1.0.15468.25-2build1_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/i/intel-graphics-compiler/libigdfcl1_1.0.15468.25-2build1_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/l/llvm-toolchain-14/libclang-cpp14t64_14.0.6-19build4_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/l/llvm-toolchain-14/libllvm14t64_14.0.6-19build4_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/o/opencl-clang-14/libopencl-clang14_14.0.0-4build2_amd64.deb'
+        'http://archive.ubuntu.com/ubuntu/pool/universe/s/spirv-llvm-translator-14/libllvmspirvlib14_14.0.0-12build1_amd64.deb'
+      )
+      mkdir -p $out/pkgs
+      cd $out/pkgs
+      for url in "''${urls[@]}"; do
+        curl --silent --show-error --fail --location --remote-name "$url"
+      done
     '';
-    outputHash = "sha256-oA+BCexa+lpTLgiqDseagUJNme5+EHp5bgBGBE/bOsk=";
+    outputHash = "sha256-vEjA1xIEGP608c1YLnBYl8ZCT3tcUns9ChtnJLiyatA=";
     outputHashMode = "recursive";
   };
   script = ''
     #!/bin/sh
     set -o errexit -o nounset
-    apt install -y /debs/*
+    apt install -y /pkgs/*
     rm -rf \
-      /debs \
+      /pkgs \
       /var/cache/ldconfig/aux-cache \
       /var/log/apt/history.log \
       /var/log/apt/term.log \
