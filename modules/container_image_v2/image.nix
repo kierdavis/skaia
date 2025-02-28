9a870c53a2ace2eeb1fc6652416aa15646dde3e2
Kier Davis <me@kierdavis.com>
2025-02-28 01:43:11 +0000

    refactor to avoid downloading nixpkgs on every container_image_v2 digest read

--- modules/container_image_v2/image.nix
+++ modules/container_image_v2/image.nix
@@ -1,5 +1,10 @@
 let
-  pkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/9ecb50d2fae8680be74c08bb0a995c5383747f89.tar.gz) {
+  bootstrapPkgs = import <nixpkgs> {};
+
+  pkgs = import (bootstrapPkgs.fetchzip {
+    url = "https://github.com/NixOS/nixpkgs/archive/9ecb50d2fae8680be74c08bb0a995c5383747f89.tar.gz";
+    hash = "sha256-b4JrUmqT0vFNx42aEN9LTWOHomkTKL/ayLopflVf81U=";
+  }) {
     overlays = [ (import ./overlay.nix) ];
   };
 
