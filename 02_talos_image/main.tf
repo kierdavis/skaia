f639d3479f4a06ad911bab3f9fb459490bc4aed0
Kier Davis <me@kierdavis.com>
2025-03-14 23:40:57 +0000

    re-image nitram back to v1.7.7
    Won't be able to use `talosctl upgrade` to get to 1.8 since
    somehow I'm running a Kubernetes version that's too new to be
    officially supported by the v1.7.x series.

--- 02_talos_image/main.tf
+++ 02_talos_image/main.tf
@@ -20,16 +20,6 @@ provider "linode" {
 
 locals {
   instances = {
-    "1.7.1" = {
-      version = "1.7.1"
-      schematic = {
-        customization = {
-          systemExtensions = {
-            officialExtensions = ["siderolabs/tailscale"]
-          }
-        }
-      }
-    }
     "1.7.7" = {
       version = "1.7.7"
       schematic = {
