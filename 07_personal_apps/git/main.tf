6c1d2f7f1e2541e7a4ce74a8636437d1223cbd3d
Kier Davis <me@kierdavis.com>
2025-04-01 16:38:05 +0100

    migrate git repositories to new storage class scheme

--- 07_personal_apps/git/main.tf
+++ 07_personal_apps/git/main.tf
@@ -91,7 +91,7 @@ resource "kubernetes_stateful_set" "main" {
       }
       spec {
         access_modes       = ["ReadWriteOnce"]
-        storage_class_name = "blk-gp0"
+        storage_class_name = "rbd-documents0"
         volume_mode        = "Filesystem"
         resources {
           requests = { storage = "1Gi" }
