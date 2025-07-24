cdcc207deac834ce7dd2920163e61825c3f9139e
Kier Davis <me@kierdavis.com>
2025-07-24 19:21:13 +0100

    transcoding: use smaller ffmpeg build until proven otherwise

--- 07_personal/transcoding/image/image.nix
+++ 07_personal/transcoding/image/image.nix
@@ -1,4 +1,4 @@
-{ ffmpeg-full, intel-media-driver, lib, libva-utils, stamp }:
+{ ffmpeg, intel-media-driver, lib, libva-utils, stamp }:
 
 stamp.fromNix {
   name = "stamp-img-skaia-transcoding";
@@ -6,5 +6,5 @@ stamp.fromNix {
     mkdir -p run/opengl-driver/lib/dri
     ln -sf ${intel-media-driver}/lib/dri/iHD_drv_video.so -t run/opengl-driver/lib/dri
   '';
-  env.PATH = lib.makeBinPath [ ffmpeg-full libva-utils ];
+  env.PATH = lib.makeBinPath [ ffmpeg libva-utils ];
 }
