3991cbf49d5c929bb716966eb97877423d3fa7b7
Kier Davis <me@kierdavis.com>
2025-07-25 00:00:02 +0100

    remove dead code

--- modules/container_image_v2/build_and_push.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env nix-shell
-#!nix-shell -p crane -i bash
-set -o errexit -o nounset -o pipefail
-
-dir=$(nix-store --realise "$derivation")
-crane push --index "$dir/oci" "$name_and_tag"
