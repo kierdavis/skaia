52643860d3c6d843506512c4dd109829e35375b6
Kier Davis <me@kierdavis.com>
2025-07-30 02:48:13 +0100

    remove dead code

--- modules/container_image/digest.nix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-query=$(cat)
-src=$(jq --raw-output .src <<<"$query")
-eval args=($(jq --raw-output '.args|fromjson|to_entries|map(["--arg",.key,.value])|flatten|@sh' <<<"$query"))
-
-generator=$(nix-build --no-out-link "${args[@]}" "$src")
-jq --raw-input --compact-output '{generator:.,digest:.|ltrimstr("/nix/store/")|split("-")|.[0]}' <<<"$generator"
--- modules/container_image/push.buildah.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
-
-podman image tag "$iid" "$tag"
-podman image push "$tag"
-podman image untag "$tag"
