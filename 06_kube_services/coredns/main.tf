10f89c9187a1ecf6ec4c39ba7d9a8029ccaf24c5
Kier Davis <me@kierdavis.com>
2025-08-19 12:41:14 +0100

    coredns: use same indentation style as the original Corefile created by Talos
    This means the changes that `talosctl upgrade-k8s` would make are a lot easier to see.

--- 06_kube_services/coredns/main.tf
+++ 06_kube_services/coredns/main.tf
@@ -14,27 +14,26 @@ resource "kubernetes_config_map" "main" {
   data = {
     Corefile = <<-EOT
       .:53 {
-        errors
-        health {
-          lameduck 5s
-        }
-        ready
-        log . {
-          class error
-        }
-        prometheus :9153
+          errors
+          health {
+              lameduck 5s
+          }
+          ready
+          log . {
+              class error
+          }
+          prometheus :9153
 
-        forward tail.skaia.cloud 100.100.100.100
-        kubernetes kube.skaia.cloud in-addr.arpa ip6.arpa {
-          pods insecure
-          fallthrough in-addr.arpa ip6.arpa
-        }
-        forward . /etc/resolv.conf
-
-        cache 30
-        loop
-        reload
-        loadbalance
+          forward tail.skaia.cloud 100.100.100.100
+          kubernetes kube.skaia.cloud in-addr.arpa ip6.arpa {
+              pods insecure
+              fallthrough in-addr.arpa ip6.arpa
+          }
+          forward . /etc/resolv.conf
+          cache 30
+          loop
+          reload
+          loadbalance
       }
     EOT
   }
